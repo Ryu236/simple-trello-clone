e1eab4382fafd85f011ceb9c82fae4f9d5aa96a4
Ryu236 <ryu.adv.2360@gmail.com>
2020-04-18 15:33:29 +0900

    :sparkles: Feat(boardCreattion): Add CreateBoardContainer.js

---
+++ src/components/boards/boardCreation/CreateBoardContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import CreateBoard from './CreateBoard';
+import ActiveCreateBoard from './ActiveCreateBoard'
+
+class CreateBoardContainer extends Components {
+
+  static propTypes = {
+    createBoard: PropTypes.object
+  }
+
+  render () {
+
+    const { createBoard } = this.props;
+
+    return (
+      <div>
+        { createBoard.isBoardOpen ? <ActiveCreateBoard /> : <CreateBoard /> }
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ createBoaed }) {
+  return {
+    createBoard
+  }
+}
+
+export default connect(mapStateToProps)(CreateBoardContainer)
