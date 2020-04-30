82b8eba1c49acfff5b8b33081508e725ff368090
Ryu236 <ryu.adv.2360@gmail.com>
2020-04-30 23:59:55 +0900

    :sparkles: Feat(Store): add Srore.js and update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it ('renders without crashing', () => {
-  const div = document.createElement('div');
+import Strore from "./Store.js";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
 });
