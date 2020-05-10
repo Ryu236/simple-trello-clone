014a2302fbcf544fd666b27af46eb94075094b12
Ryu236 <ryu.adv.2360@gmail.com>
2020-05-10 21:10:37 +0900

    :bug: Fix(Reducers): fix type and update eslintrc.json

--- src/Reducers/RootReducer.js
+++ src/Reducers/RootReducer.js
@@ -1,12 +1,12 @@
-import { combineReducers } from 'redux';
-import CreateBoardReducer from './CreateBoardReducer';
-import BoardReducer from './BoardReducer';
-import { resucer as formReducer } from 'redux-form';
+import { combineReducers } from "redux";
+import CreateBoardReducer from "./CreateBoardReducer";
+import BoardReducer from "./BoardReducer";
+import { reducer as formReducer } from "redux-form";
 
 const RootReducer = combineReducers({
   createBoard: CreateBoardReducer,
   board: BoardReducer,
-  form: formReducer
-})
+  form: formReducer,
+});
 
-export default RootReducer
+export default RootReducer;
